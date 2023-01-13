ba3e1c79e97cda425e3fec50d4ffb49a3c8fe52e
sharvil <sharvilbhalke@gmail.com>
2023-01-13 22:34:08 +0530

    Changed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// import {Route, Link } from 'react-router-dom';
+import {Route, Link, Switch } from 'react-router-dom';
 
-function App() {
+import { Layout, Typography, Space } from 'antd';
+
+import {Navbar, Exchanges , Homepage,Cryptocurrencies, CryptoDetails, News } from './components';
+
+import './App.css';
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+        <div className='navbar'>
+          <Navbar/>
+        </div>
+        <div className='main'>
+          <Layout>
+            <div className='routes'>
+              <Switch>
+                <Route exact path='/'>
+                  <Homepage />
+                </Route>
+                <Route exact path='/exchanges'>
+                  <Exchanges />
+                </Route>
+                <Route exact path='/cryptocurrencies'>
+                  <Cryptocurrencies />
+                </Route>
+                <Route exact path='/crypto/:coinId'>
+                  <CryptoDetails />
+                </Route>
+                <Route exact path='/news'>
+                  <News />
+                </Route>
+              </Switch>
+              {/* <BrowserRouter>
+                <Routes>
+                  <Route path="/" element={<Homepage />} />
+                </Routes>
+                <Routes>
+                  <Route path="/exchange" element={<Exchanges />} />
+                </Routes>
+                <Routes>
+                  <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+                </Routes>
+                <Routes>
+                  <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+                </Routes>
+                <Routes>
+                  <Route path="/news" element={<News />} />
+                </Routes>
+              </BrowserRouter> */}
+            </div>
+          </Layout>
+          <div className='footer'>
+            <Typography.Title level={5} style={{color:'white', textAlign:'center'}}>
+              Cryptomania <br/>
+              All rights reserved
+            </Typography.Title>
+            <Space>
+              <Link to='/'>Home</Link>
+              <Link to='/exchanges'>Exchanges</Link>
+              <Link to='/news'>News</Link>
+            </Space>
+          </div>
+        </div>
+        
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
