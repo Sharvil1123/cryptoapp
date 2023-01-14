5a0a85b52724aea2ac34aa3df794040045d74f19
sharvil <sharvilbhalke@gmail.com>
2023-01-14 19:07:57 +0530

    added redux store, started working on data fetchin

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import  ReactDOM  from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import 'antd/dist/antd-with-locales';
+import { Provider } from "react-redux";
+import store from "./app/store";
 
-
-ReactDOM.render(<Router><App/></Router>, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </Router>,
+    document.getElementById('root')
+);
---
+++ src/services/cryptoAPI.js
@@ -0,0 +1,9 @@
+// const options = {
+//     method: 'GET',
+//     url: 'https://coinranking1.p.rapidapi.com/coin/Qwsogvtv82FCd/supply',
+//     headers: {
+//       'X-RapidAPI-Key': '0f3f83c853msh458acb7e0c0c36cp1b52dajsnd622eb17f5d1',
+//       'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+//     }
+//   };
+//Get coin supply
